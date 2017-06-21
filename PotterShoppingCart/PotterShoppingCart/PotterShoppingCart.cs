45a395901de622fac9c188abc11b34d396140442
kokobobo <kobo.huang@liteon.com>
2017-06-21 10:54:48 +0800

    第三個綠燈 Test_買_第一集_一本_第二集_一本_第三集_一本_價格為_270

--- PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PotterShoppingCart
@@ -16,16 +17,49 @@ namespace PotterShoppingCart
         public int CountShoppingCartPrice(IEnumerable<BookOrderData> orders)
         {
             var totalPrice = 0;
-            var setAmount =
-                (from order in orders
-                 select order.Amount)
-                .Min();
+            var orderGroupList = new List<List<int>>();
 
-            totalPrice += (int)(setAmount * orders.Count() * BOOK_ORIGINALPRICE * 0.95);
-
+            // 把資料分成不重複的一組
             foreach (var order in orders)
             {
-                totalPrice += (order.Amount - setAmount) * BOOK_ORIGINALPRICE;
+                for (int i = 0; i < order.Amount; i++)
+                {
+                    if (orderGroupList.Count <= i)
+                    {
+                        var orderGroup = new List<int>();
+                        orderGroup.Add(order.Volumn);
+                        orderGroupList.Add(orderGroup);
+                    }
+                    else
+                    {
+                        orderGroupList[i].Add(order.Volumn);
+                    }
+                }
+            }
+
+            // 尋訪所有組別後依個別折扣計算總價
+            foreach (var orderGroup in orderGroupList)
+            {
+                switch (orderGroup.Count)
+                { 
+                 case 1: // 原價
+
+                    totalPrice += orderGroup.Count * BOOK_ORIGINALPRICE;
+                    break;
+
+                case 2: // 2本95折
+
+                    totalPrice += (int)(orderGroup.Count * BOOK_ORIGINALPRICE * 0.95);
+                    break;
+
+                case 3: // 3本9折
+
+                    totalPrice += (int)(orderGroup.Count * BOOK_ORIGINALPRICE * 0.9);
+                    break;
+
+                default:
+                    throw new Exception("This order is illegal (unknown book?)");
+                }
             }
 
             return totalPrice;
