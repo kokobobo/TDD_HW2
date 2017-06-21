ccb18daec4a5abab86ed4b2a5317b569a51440c4
kokobobo <kobo.huang@liteon.com>
2017-06-21 10:44:54 +0800

    第三個紅燈 Test_買_第一集_一本_第二集_一本_第三集_一本_價格為_270

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -43,5 +43,24 @@ namespace PotterShoppingCart.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_買_第一集_一本_第二集_一本_第三集_一本_價格為_270()
+        {
+            // arrange
+            var orders = new List<BookOrderData>()
+            {
+                new BookOrderData() { Volumn = 1, Amount = 1},
+                new BookOrderData() { Volumn = 2, Amount = 1},
+                new BookOrderData() { Volumn = 3, Amount = 1}
+            };
+
+            var target = new PotterShoppingCart();
+            var expected = 270;
+            // act
+            var actual = target.CountShoppingCartPrice(orders);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
