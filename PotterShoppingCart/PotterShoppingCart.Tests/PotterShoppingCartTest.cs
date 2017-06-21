2205092babaab0b05829dfbb82db303a97218674
kokobobo <kobo.huang@liteon.com>
2017-06-21 11:06:34 +0800

    第七個綠燈 Test_買_第一集_一本_第二集_兩本_第三集_兩本_價格為_460

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -103,5 +103,43 @@ namespace PotterShoppingCart.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_買_第一集_一本_第二集_一本_第三集_兩本_價格為_370()
+        {
+            // arrange
+            var orders = new List<BookOrderData>()
+            {
+                new BookOrderData() { Volumn = 1, Amount = 1},
+                new BookOrderData() { Volumn = 2, Amount = 1},
+                new BookOrderData() { Volumn = 3, Amount = 2},
+            };
+
+            var target = new PotterShoppingCart();
+            var expected = 370;
+            // act
+            var actual = target.CountShoppingCartPrice(orders);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_買_第一集_一本_第二集_兩本_第三集_兩本_價格為_460()
+        {
+            // arrange
+            var orders = new List<BookOrderData>()
+            {
+                new BookOrderData() { Volumn = 1, Amount = 1},
+                new BookOrderData() { Volumn = 2, Amount = 2},
+                new BookOrderData() { Volumn = 3, Amount = 2},
+            };
+
+            var target = new PotterShoppingCart();
+            var expected = 460;
+            // act
+            var actual = target.CountShoppingCartPrice(orders);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
