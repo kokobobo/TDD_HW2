eb3d067b6cc8c89dc065ca941124452690e08e9b
kokobobo <kobo.huang@liteon.com>
2017-06-21 10:38:20 +0800

    真 第二個綠燈 Test_買_第一集_一本_第二集_一本_價格為_190

--- PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PotterShoppingCart
 {
@@ -10,32 +8,25 @@ namespace PotterShoppingCart
         public int Volumn { get; set; }
         public int Amount { get; set; }
     }
+
     public class PotterShoppingCart
     {
         private const int BOOK_ORIGINALPRICE = 100;
+
         public int CountShoppingCartPrice(IEnumerable<BookOrderData> orders)
         {
             var totalPrice = 0;
-            var setAmount = 0;
-            foreach (var order in orders)
-            {
-                if (setAmount == 0)
-                {
-                    setAmount = order.Amount;
-                }
-                else
-                {
-                    setAmount = Math.Min(setAmount, order.Amount);
-                }
-            }
+            var setAmount =
+                (from order in orders
+                 select order.Amount)
+                .Min();
 
-            totalPrice += (int)(setAmount * 2 * BOOK_ORIGINALPRICE * 0.95);
+            totalPrice += (int)(setAmount * orders.Count() * BOOK_ORIGINALPRICE * 0.95);
 
             foreach (var order in orders)
             {
                 totalPrice += (order.Amount - setAmount) * BOOK_ORIGINALPRICE;
             }
-            
 
             return totalPrice;
         }
