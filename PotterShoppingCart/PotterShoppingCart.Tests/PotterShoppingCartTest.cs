128452d0a32e1d7e0d5a68e832a74ed8e6f22036
kokobobo <kobo.huang@liteon.com>
2017-06-21 11:00:59 +0800

    第五個紅燈  Test_買_第一集_一本_第二集_一本_第三集_一本_第四集_一本_第五集_一本_價格為_375

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -82,5 +82,26 @@ namespace PotterShoppingCart.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_買_第一集_一本_第二集_一本_第三集_一本_第四集_一本_第五集_一本_價格為_375()
+        {
+            // arrange
+            var orders = new List<BookOrderData>()
+            {
+                new BookOrderData() { Volumn = 1, Amount = 1},
+                new BookOrderData() { Volumn = 2, Amount = 1},
+                new BookOrderData() { Volumn = 3, Amount = 1},
+                new BookOrderData() { Volumn = 4, Amount = 1},
+                new BookOrderData() { Volumn = 5, Amount = 1}
+            };
+
+            var target = new PotterShoppingCart();
+            var expected = 375;
+            // act
+            var actual = target.CountShoppingCartPrice(orders);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
